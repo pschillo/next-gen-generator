478dd1c70143dbb566f81ce3b37a3ec7bbf51f59
pschillo <pschillo@uni-osnabrueck.de>
2021-07-05 19:26:17 +0200

    general process update

--- process.py
+++ process.py
@@ -6,14 +6,21 @@ import csv
 # specify path to material
 path = "materials\chakoteya\StarTrek_NextGen_transcript_complete.txt"
 
-## read Picard's lines from script into csv
-with open(path) as script:
-    with open("quotes_PICARD.csv", "w") as file:
-        for line in script:
-            if line.startswith("PICARD"):
-                print(line)
-            #with open("quotes_PICARD.csv", "wb") as file:
-                file.write(line)
-                file.write("\n")
-                #writer = csv.writer(file)
-                #writer.writerow(line)
+# specify list of characters
+list = ["PICARD", "DATA", "Q", "RIKER", "TROI"]
+
+# process raw text for each character
+for name in list:
+
+    # create file name
+    title = "quotes_%s.csv" % name
+    #print(title)
+
+    # read lines from script into csv
+    with open(path) as script:
+        with open(title, "w") as file:
+            for line in script:
+                if line.startswith(name):
+                    #print(line)
+                    file.write(line)
+                    file.write("\n")
