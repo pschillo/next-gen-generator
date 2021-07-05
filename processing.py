de62c13ab263611af6ba0fe1ec64eb2ad99fd907
pschillo <pschillo@uni-osnabrueck.de>
2021-07-06 01:21:03 +0200

    Das Schweigen der Lemmas

--- processing.py
+++ processing.py
@@ -26,13 +26,51 @@ def process(list,path):
                         file.write(line)
                         file.write("\n")
 
-def keywords():
-    
+# paths to newly created .csv's (same order as characters in 'list'!)
+filepaths = ["quotes_PICARD.csv", "quotes_DATA.csv", "quotes_Q.csv", "quotes_RIKER.csv", "quotes_TROI.csv"]
+testlist = ["quotes_PICARD.csv"]
+
+def keywords(list):
+
+    # process each of the files from the list
+    for path in list:
+
+        # read file
+        with open(path) as file:
+            filereader = csv.reader(file)
+            #print("file was read")
+
+            # per row
+            for row in filereader:
+
+                # concatenate row to string
+                string = "".join(str(x) for x in row)
+                #print(string)
+
+                # load spacy
+                nlp = spacy.load("en_core_web_sm")
+                doc = nlp(string)
+
+                # collect lemmas
+                lemmas = [word.lemma_ for word in doc]
+                #for word in doc:
+                #lemmas.append(row.lemma_)
+                print(lemmas)
+
+                # add lemmas as new column
+                ##row.append(lemmas)
+
+            print("this is where the keyword function goes")
 
 def main():
 
     # process raw data
-    process(list,path)
+    #process(list,path)
+    print("raw data has been processed")
+
+    # test keyword function
+    keywords(testlist)
+    print("keyword function has been tested")
 
 if __name__ == "__main__":
     main()
